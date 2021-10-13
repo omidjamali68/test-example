0dd4c747f84e7912770be1ee467780b59241c731
omidjamali68 <o.jamali2016@gmail.com>
2021-10-13 22:24:46 +0330

    seed data ingredientUnits and recipeCategories done

--- src/Cooking.Migrations/202105061313_Initial_SeedData.cs
+++ src/Cooking.Migrations/202105061313_Initial_SeedData.cs
@@ -2,18 +2,23 @@
 
 namespace Cooking.Migrations
 {
-    //[Migration(202105061313)]
-    //public class _202105061313_Initial_Iran_Data_Inserted : Migration
-    //{
-    //    public override void Up()
-    //    {
-    //        Execute.EmbeddedScript("IranGeoData.sql");
-    //    }
+    [Migration(202105061313)]
+    public class _202105061313_Initial_Iran_Data_Inserted : Migration
+    {
+        public override void Up()
+        {
+            Execute.EmbeddedScript("IranGeoData.sql");
+            Execute.EmbeddedScript("InitialIngredientUnits.sql");
+            Execute.EmbeddedScript("InitialRecipeCategories.sql");
 
-    //    public override void Down()
-    //    {
-    //        Execute.Sql("DELETE FROM Cities;");
-    //        Execute.Sql("DELETE FROM Provinces;");
-    //    }
-    //}
+        }
+
+        public override void Down()
+        {
+            Execute.Sql("DELETE FROM RecipeCategories;");
+            Execute.Sql("DELETE FROM IngredientUnits;");
+            Execute.Sql("DELETE FROM Cities;");
+            Execute.Sql("DELETE FROM Provinces;");
+        }
+    }
 }
