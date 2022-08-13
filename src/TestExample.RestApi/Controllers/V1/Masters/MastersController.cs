01f29687f70f0f1b7a2bae3f5dd1b322e5496b32
Omid Jamali <omid.jamali_68@yahoo.com>
2022-08-13 10:29:58 +0430

    Master controller some bug fix

--- src/TestExample.RestApi/Controllers/V1/Masters/MastersController.cs
+++ src/TestExample.RestApi/Controllers/V1/Masters/MastersController.cs
@@ -1,10 +1,15 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using TestExample.Infrastructure.Application;
 using TestExample.Services.Masters.Contracts;
 
 namespace TestExample.RestApi.Controllers.V1.Masters
 {
+    [ApiController]
+    [ApiVersion("1.0")]
     [Route("/api/v{version:apiVersion}/masters")]
+    [Authorize(Roles = UserRoles.Admin)]
     public class MastersController : Controller
     {
         private readonly IMasterService _service;
